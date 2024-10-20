5eb47df8a5e81490c1633b3531bc87dae421c158
kunal nagwanshi <nagwanshikunal23@gmail.com>
2024-10-20 21:22:55 +0530

    first comit

---
+++ index.js
@@ -0,0 +1,5 @@
+document.querySelector('input').click();
+document.firstElementChild.lastElementChild.querySelector('ul').lastElementChild.innerHTML="kunal";
+// document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Angela"
+
+//document.firstElementChild.lastElementChild.querySelector('ul').firstElementChild.innerHTML="nagwanshi";
