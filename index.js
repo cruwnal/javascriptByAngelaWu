38d2444207083cb7358c887f4f650c28cdf311a8
kunal nagwanshi <nagwanshikunal23@gmail.com>
2024-10-21 23:48:08 +0530

    choosing all the buttons using loop

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-document.querySelector("button").addEventListener("click",handleClick)
-function handleClick(){
-alert("i am clicked")
+// document.querySelector("button").addEventListener("click",handleClick)
+// function handleClick(){
+// alert("i am clicked")
+// }
+
+
+//annoymous function 
+
+let nodeListButton = document.querySelectorAll("button");
+
+for (let i =0 ; i<nodeListButton.length;i++){
+
+    nodeListButton[i].addEventListener("click",function () {
+        alert("button got clicked")
+    })
 }
