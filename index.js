415b1ccbd50b5062dedc5be735b225adef939b1c
kunal nagwanshi <nagwanshikunal23@gmail.com>
2024-10-25 22:26:25 +0530

    adding music

--- index.js
+++ index.js
@@ -11,9 +11,27 @@ console.log(nodeListButton);
 for (let i =0 ; i<nodeListButton.length;i++){
 
     nodeListButton[i].addEventListener("click",function () {
-
+     var buttonClick = this.innerHTML;
+      var playedMusic ='';
+        switch(buttonClick){
+            case'w':playedMusic='sounds/tom-1.mp3';
+            break;
+            case 'a':playedMusic='sounds/tom-2.mp3'
+            break;
+            case 's':playedMusic='sounds/tom-3.mp3'
+            break;
+            case 'd':playedMusic='sounds/tom-4.mp3'
+            break;
+            case 'j':playedMusic='sounds/snare.mp3'
+            break;
+            case 'k':playedMusic='sounds/kick-bass.mp3'
+            break;   
+            case 'l':playedMusic='sounds/crash.mp3'
+            break;                 
+           
+        }
         
-       var audio = new Audio('sounds/tom-1.mp3');
+       var audio = new Audio(playedMusic);
        audio.play();
     })
 }
